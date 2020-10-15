22058356b63b1abbca180f0634e10e42d61f66c3
PriyanshBordia <priyanshbordia1@gmail.com>
2020-10-16 00:42:12 +0530

    deploy changes

--- airlines/settings.py
+++ airlines/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "thisisasecretkey"
+SECRET_KEY = config('SECRET_KEY ')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['https://the-milky-way-airlines.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
