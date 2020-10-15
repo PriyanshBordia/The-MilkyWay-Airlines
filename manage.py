ca4c17b19d40cbd59f352f6b28211ab93e1523b2
PriyanshBordia <priyanshbordia1@gmail.com>
2020-10-16 01:16:34 +0530

    changed filename

--- manage.py
+++ manage.py
@@ -5,11 +5,12 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'airlines.settings')
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'the_milkyway_airlines.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        raise ImportError (
+        raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
