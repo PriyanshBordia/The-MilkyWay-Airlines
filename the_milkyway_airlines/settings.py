dc71c342e26eba47e34ffbc39bc27ab41fed68f4
PriyanshBordia <priyanshbordia1@gmail.com>
2020-10-19 10:57:46 +0530

    c8

--- the_milkyway_airlines/settings.py
+++ the_milkyway_airlines/settings.py
@@ -171,7 +171,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'flights/static'),
 )
 
 #login redirection to homepage
