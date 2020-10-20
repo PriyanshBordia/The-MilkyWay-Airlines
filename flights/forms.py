c10e2ff5b2344cf6e6858b29b33022388e0f1c6c
PriyanshBordia <priyanshbordia1@gmail.com>
2020-10-20 23:05:16 +0530

    Update forms.py

--- flights/forms.py
+++ flights/forms.py
@@ -3,7 +3,6 @@ from django import forms
 from .models import Passenger
 
 
-    e-mail = forms.EmailField()
 class PassResetConfirmation(forms.Form):
     name = forms.CharField()
     email = forms.EmailField(labe = "E-mail")
