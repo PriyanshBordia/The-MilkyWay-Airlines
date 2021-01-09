03975610151e123544032a99923b8ab99292dabb
PriyanshBordia <priyanshbordia1@gmail.com>
2021-01-09 19:24:19 +0530

    Update settings.py

--- the_milkyway_airlines/settings.py
+++ the_milkyway_airlines/settings.py
@@ -17,7 +17,6 @@ environ.Env.read_env()
 import os
 from pathlib import Path
 
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
